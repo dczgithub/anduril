49a8cbed38493c6c135f32d0140bb71e30e2788e
dczgithub <63542885+dczgithub@users.noreply.github.com>
2025-05-21 00:57:27 +0800

    save_config
    save_config

--- ui/anduril/strobe-modes.c
+++ ui/anduril/strobe-modes.c
@@ -118,11 +118,6 @@ uint8_t strobe_state(Event event, uint16_t arg) {
 
         return EVENT_HANDLED;
     }
-    // release hold: save new strobe settings
-    else if (event == EV_click2_hold_release) {
-        save_config();
-        return EVENT_HANDLED;
-    }
 
     // click, click, hold: change speed (go slower)
     else if (event == EV_click3_hold) {
@@ -146,7 +141,7 @@ uint8_t strobe_state(Event event, uint16_t arg) {
         return EVENT_HANDLED;
     }
     // release hold: save new strobe settings
-    else if (event == EV_click3_hold_release) {
+    else if (event == EV_click2_hold_release||EV_click3_hold_release) {
         save_config();
         return EVENT_HANDLED;
     }
