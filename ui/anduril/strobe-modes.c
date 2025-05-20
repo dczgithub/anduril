54e90f75fc22533c2762d2ea4cab7fb8502f3692
dczgithub <63542885+dczgithub@users.noreply.github.com>
2025-05-21 02:26:44 +0800

    up3
    up3

--- ui/anduril/strobe-modes.c
+++ ui/anduril/strobe-modes.c
@@ -38,8 +38,45 @@ uint8_t strobe_state(Event event, uint16_t arg) {
         return EVENT_HANDLED;
     }
   
+    ////////// Every action below here is blocked in the Extended Simple UI //////////
+    #ifdef USE_SIMPLE_UI
+    if (cfg.simple_ui_active) {
+        return EVENT_HANDLED;
+    }
+    #endif  // ifdef USE_SIMPLE_UI
 
-
+    // 5 clicks: rotate through strobe/flasher modes
+    else if (event == EV_5clicks) {
+        current_strobe_type = cfg.strobe_type = (st + 1) % NUM_STROBES;
+        save_config();
+        return EVENT_HANDLED;
+    }
+    #if (NUM_CHANNEL_MODES > 1) && defined(USE_CHANNEL_PER_STROBE)
+    // 6 clicks: rotate through channel modes for the current strobe
+    else if (event == EV_6clicks) {
+        // TODO: maybe skip aux modes?
+        set_channel_mode((channel_mode + 1) % NUM_CHANNEL_MODES);
+        cfg.strobe_channels[st] = channel_mode;
+        save_config();
+        return EVENT_HANDLED;
+    }
+    #endif
+    // 7 clicks: rotate backward through strobe/flasher modes
+    else if (event == EV_7clicks) {
+        current_strobe_type = cfg.strobe_type = (st - 1 + NUM_STROBES) % NUM_STROBES;
+        save_config();
+        return EVENT_HANDLED;
+    }
+    #ifdef USE_MOMENTARY_MODE
+    // 9 clicks: go to momentary mode (momentary strobe)
+    else if (event == EV_9clicks) {
+        set_state(momentary_state, 0);
+        set_level(0);
+        return EVENT_HANDLED;
+    }
+    #endif
+    
+    if (0) {}  // placeholder
     // hold: change speed (go faster)
     //       or change brightness (brighter)
     else if (event == EV_click1_hold) {
@@ -148,43 +185,8 @@ uint8_t strobe_state(Event event, uint16_t arg) {
 
 
 
-    ////////// Every action below here is blocked in the Extended Simple UI //////////
-    #ifdef USE_SIMPLE_UI
-    if (0) {
-        return EVENT_HANDLED;
-    }
-    #endif  // ifdef USE_SIMPLE_UI
 
-    // 5 clicks: rotate through strobe/flasher modes
-    else if (event == EV_5clicks) {
-        current_strobe_type = cfg.strobe_type = (st + 1) % NUM_STROBES;
-        save_config();
-        return EVENT_HANDLED;
-    }
-    #if (NUM_CHANNEL_MODES > 1) && defined(USE_CHANNEL_PER_STROBE)
-    // 6 clicks: rotate through channel modes for the current strobe
-    else if (event == EV_6clicks) {
-        // TODO: maybe skip aux modes?
-        set_channel_mode((channel_mode + 1) % NUM_CHANNEL_MODES);
-        cfg.strobe_channels[st] = channel_mode;
-        save_config();
-        return EVENT_HANDLED;
-    }
-    #endif
-    // 7 clicks: rotate backward through strobe/flasher modes
-    else if (event == EV_7clicks) {
-        current_strobe_type = cfg.strobe_type = (st - 1 + NUM_STROBES) % NUM_STROBES;
-        save_config();
-        return EVENT_HANDLED;
-    }
-    #ifdef USE_MOMENTARY_MODE
-    // 9 clicks: go to momentary mode (momentary strobe)
-    else if (event == EV_9clicks) {
-        set_state(momentary_state, 0);
-        set_level(0);
-        return EVENT_HANDLED;
-    }
-    #endif
+
     #if defined(USE_LIGHTNING_MODE) || defined(USE_CANDLE_MODE)
 
     // clock tick: bump the random seed
